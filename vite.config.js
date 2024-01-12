111cbc610a0677ef2ec7259ec8363f68ff530227
Arttapon <152834864+Arttapon@users.noreply.github.com>
2024-01-12 23:04:50 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/Arttapon/SNRU-TodoList.git"
+  base: "/SNRU-TodoList/"
 })
 
